6fa436ceee808c07552892fd5137b02eba462e88
ferrouklilia <125090363+liliaferrouk@users.noreply.github.com>
2023-07-08 12:35:01 +0100

    securing the APIs
    - enabled token authentication
    - added an API from where we can generate tokens
    - secured the APIs so that only authenticated user can access them
    - installed djoser and enabled its endpoints (creat a user, login logout ...)

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
 
@@ -9,14 +10,17 @@ def index(request):
 
 #using generics:
 class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]  #only authenticated users can see this view
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 #using viewsets:
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
